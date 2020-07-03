363e80ec181653310f85dcbca99288c07200e9d6
UtmoD <tu.dominh2k@gmail.com>
2020-07-03 10:31:20 +0700

    fix architecture encoding

--- source/genetic_CNN.py
+++ source/genetic_CNN.py
@@ -18,20 +18,23 @@ def conv_layer(X, num_f, k_size):
     return node
 
 
-def do_something(a0, code, num_nodes, num_f, k_size):
+def do_something(X, code, num_nodes, num_f, k_size):
+    a0 = conv_layer(X, num_f, k_size)
     nodes = {'a1': conv_layer(a0, num_f, k_size)}
 
     for node in range(2, num_nodes + 1):
-        nodes['a' + str(node)] = conv_layer(nodes['a' + str(node - 1)],
-                                            num_f, k_size)
-
+        nodes['a' + str(node)] = a0
         end_idx = code_length_of(node)
         start_idx = end_idx - (node - 1)
         prev_nodes = code[start_idx: end_idx]
         connected_nodes = np.where(prev_nodes == 1)[0] + 1  # increment index number
         for prev_node in connected_nodes:
-            nodes['a' + str(node)] = Add()([nodes['a' + str(node)],
-                                            nodes['a' + str(prev_node)]])
+            if (prev_node == node-1):
+                nodes['a' + str(node)] = conv_layer(nodes['a' + str(node - 1)],
+                                                    num_f, k_size)
+            else:
+                nodes['a' + str(node)] = Add()([nodes['a' + str(node)],
+                                                nodes['a' + str(prev_node)]])
 
     # Get node last
     node_L = conv_layer(nodes['a' + str(num_nodes)], num_f, k_size)
@@ -78,41 +81,41 @@ def genetic_model(architecture, hyper_params,
                   optimizer=hyper_params['optimizer'], metrics=['accuracy'])
     return model
 
-import timeit
-from keras.datasets import cifar10
-from keras.utils import to_categorical
-architecture = { 'stages' : [('Stage1', 3), ('Stage2', 5)], 
-                 'code' : np.array([0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1]),
-                }
-hyper_params = { 'optimizer' : 'adam',
-                 'drop out' : 0.5,
-                 'epochs' : 20,
-                 'kernel size' : (5, 5),
-                 'pool size' : 2,
-                 'strides' : 2,
-                 'filters' : 20,
-                 'fc units': 500,
-                 'pooling' : 'max'}
+# import timeit
+# from keras.datasets import cifar10
+# from keras.utils import to_categorical
+# architecture = { 'stages' : [('Stage1', 3), ('Stage2', 5)], 
+#                  'code' : np.array([0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1]),
+#                 }
+# hyper_params = { 'optimizer' : 'adam',
+#                  'drop out' : 0.5,
+#                  'epochs' : 20,
+#                  'kernel size' : (5, 5),
+#                  'pool size' : 2,
+#                  'strides' : 2,
+#                  'filters' : 20,
+#                  'fc units': 500,
+#                  'pooling' : 'max'}
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-# Convert class vectors to binary class matrices.
-num_classes = 10
-y_train = to_categorical(y_train, num_classes)
-y_test = to_categorical(y_test, num_classes)
+# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+# # Convert class vectors to binary class matrices.
+# num_classes = 10
+# y_train = to_categorical(y_train, num_classes)
+# y_test = to_categorical(y_test, num_classes)
 
-# Data normalization
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+# # Data normalization
+# x_train = x_train.astype('float32')
+# x_test = x_test.astype('float32')
+# x_train /= 255
+# x_test /= 255
 
 
-model = genetic_model(architecture, hyper_params,
-                     input_shape=x_train.shape[1:], classes=num_classes)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.summary()
-#model.save('./models/sga')
-start = timeit.default_timer()
-model.fit(x=x_train, y=y_train, epochs=20, validation_data=(x_test, y_test))
-end = timeit.default_timer()
-print('Elapsed time: {}'.format(end - start))
+# model = genetic_model(architecture, hyper_params,
+#                      input_shape=x_train.shape[1:], classes=num_classes)
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# model.summary()
+# #model.save('./models/sga')
+# start = timeit.default_timer()
+# model.fit(x=x_train, y=y_train, epochs=20, validation_data=(x_test, y_test))
+# end = timeit.default_timer()
+# print('Elapsed time: {}'.format(end - start))
